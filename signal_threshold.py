44038cfdcf57b8892f088f1abd6ffb2b7a11c0d4
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2026-04-08 18:36:01 +0200

    Create signal_threshold.py

---
+++ signal_threshold.py
@@ -0,0 +1,27 @@
+# a_signal_threshold.py
+
+THRESHOLDS = {
+    # ===== SPECIFICHE =====
+    "ethereum-bsc": {
+        "min_spread": 4.5,
+    },
+    "bsc-ethereum": {
+        "min_spread": 5,
+    },
+    "ethereum-base": {
+        "min_spread": 4.5,
+    },
+    "base-ethereum": {
+        "min_spread": 5,
+    },
+    "bsc-base": {
+        "min_spread": 3,
+    },
+    "base-bsc": {
+        "min_spread": 3,
+    },
+    # ===== DEFAULT =====
+    "default": {
+        "min_spread": 3,
+    },
+}
